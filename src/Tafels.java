e347ff02a20e9138191e341923e7ec5ae476f26d
YoussraHazzout <YoussraHazzout@gmail.com>
2020-09-24 11:29:49 +0200

    Versie2.0

--- src/Tafels.java
+++ src/Tafels.java
@@ -1,37 +1,36 @@
-public class Tafels<getal> { public static void main(String args[]);
-int getal = 8;
-           int uitkomst1= 1*getal;
-            int uitkomst2= 2*getal;
-            int uitkomst3= 3*getal;
-            int uitkomst4= 4*getal;
-            int uitkomst5= 5*getal;
-            int uitkomst6= 6*getal;
-            int uitkomst7= 7*getal;
-            int uitkomst8= 8*getal;
-            int uitkomst9= 9*getal;
-            int uitkomst10= 10*getal;
+public class Tafels {
 
-    System.out.println(1 * int getal = int uitkomst1);
+    public static void main(String[] Tafels){
 
-    System.out.println(2 * int getal= int uitkomst2);
+    int getal = 8;
+    int uitkomst1 = 1 * getal;
+    int uitkomst2 = 2 * getal;
+    int uitkomst3 = 3 * getal;
+    int uitkomst4 = 4 * getal;
+    int uitkomst5 = 5 * getal;
+    int uitkomst6 = 6 * getal;
+    int uitkomst7 = 7 * getal;
+    int uitkomst8 = 8 * getal;
+    int uitkomst9 = 9 * getal;
+    int uitkomst10 = 10 * getal;
 
-    System.out.println(3 * int getal= int uitkomst3);
+    System.out.println("1 x " + getal + "=" + uitkomst1);
+    System.out.println("2 x " + getal + "=" + uitkomst2);
+    System.out.println("3 x " + getal + "=" + uitkomst3);
+    System.out.println("4 x " + getal + "=" + uitkomst4);
+    System.out.println("5 x " + getal + "=" + uitkomst5);
+    System.out.println("6 x " + getal + "=" + uitkomst6);
+    System.out.println("7 x " + getal + "=" + uitkomst7);
+    System.out.println("8 x " + getal + "=" + uitkomst8);
+    System.out.println("9 x " + getal + "=" + uitkomst9);
+    System.out.println("10 x " + getal + "=" + uitkomst10);
 
-    System.out.println(4 * int getal= int uitkomst4);
 
-    System.out.println(5 * int getal= int uitkomst5);
 
-    System.out.println(6 * int getal= int uitkomst6);
-
-    System.out.println(7 * int getal= int uitkomst7);
-
-    System.out.println(8 * int getal= int uitkomst8);
-
-    System.out.println(9 * int getal= int uitkomst9);
-
-    System.out.println(10 * int getal= int uitkomst10);
-
+            // andere manier?
+    /*
     System.out.println();
+
     System.out.println("1 * 8= 8");
     System.out.println("2 * 8= 16");
     System.out.println("3 * 8= 24");
@@ -42,5 +41,6 @@ int getal = 8;
     System.out.println("8 * 8= 64");
     System.out.println("9 * 8= 72");
     System.out.println("10 * 8= 80");
+    */
 }
 }
--- src/java/var.java
+++
@@ -1,4 +0,0 @@
-package java;
-
-public class var {8
-}
