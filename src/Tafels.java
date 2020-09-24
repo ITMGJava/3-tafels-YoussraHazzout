7bf378daf67da3bcd8cdf4243d844407431f2957
YoussraHazzout <YoussraHazzout@gmail.com>
2020-09-24 10:30:03 +0200

    Versie1.0

--- src/Tafels.java
+++ src/Tafels.java
@@ -1,2 +1,36 @@
-public class Tafels {
+public class Tafels<getal> {
+    public static void main(String args[]);
+int getal = 8;
+           int uitkomst1= 1*getal;
+            int uitkomst2= 2*getal;
+            int uitkomst3= 3*getal;
+            int uitkomst4= 4*getal;
+            int uitkomst5= 5*getal
+            int uitkomst6= 6*getal;
+            int uitkomst7= 7*getal;
+            int uitkomst8= 8*getal;
+            int uitkomst9= 9*getal;
+            int uitkomst10 = 10*getal;
+
+    System.out.println(1*getal = uitkomst1);
+
+    System.out.println(2*getal= uitkomst2);
+
+    System.out.println(3*getal= uitkomst3);
+
+    System.out.println(4*getal= uitkomst4);
+
+    System.out.println(5*getal= uitkomst5);
+
+    System.out.println(6*getal= uitkomst6);
+
+    System.out.println(7*getal= uitkomst7);
+
+    System.out.println(8*getal= uitkomst8);
+
+    System.out.println(9*getal= uitkomst9);
+
+    System.out.println(10*getal= uitkomst10);
+
+}
 }
