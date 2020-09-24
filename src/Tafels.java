d73c30432c6c37e357d9952f6351bf7581a3afca
YoussraHazzout <YoussraHazzout@gmail.com>
2020-09-24 10:46:50 +0200

    Versie1.2

--- src/Tafels.java
+++ src/Tafels.java
@@ -1,36 +1,46 @@
-public class Tafels<getal> {
-    public static void main(String args[]);
+public class Tafels<getal> { public static void main(String args[]);
 int getal = 8;
            int uitkomst1= 1*getal;
             int uitkomst2= 2*getal;
             int uitkomst3= 3*getal;
             int uitkomst4= 4*getal;
-            int uitkomst5= 5*getal
+            int uitkomst5= 5*getal;
             int uitkomst6= 6*getal;
             int uitkomst7= 7*getal;
             int uitkomst8= 8*getal;
             int uitkomst9= 9*getal;
-            int uitkomst10 = 10*getal;
+            int uitkomst10= 10*getal;
 
-    System.out.println(1*getal = uitkomst1);
+    System.out.println(1 * int getal = int uitkomst1);
 
-    System.out.println(2*getal= uitkomst2);
+    System.out.println(2 * int getal= int uitkomst2);
 
-    System.out.println(3*getal= uitkomst3);
+    System.out.println(3 * int getal= int uitkomst3);
 
-    System.out.println(4*getal= uitkomst4);
+    System.out.println(4 * int getal= int uitkomst4);
 
-    System.out.println(5*getal= uitkomst5);
+    System.out.println(5 * int getal= int uitkomst5);
 
-    System.out.println(6*getal= uitkomst6);
+    System.out.println(6 * int getal= int uitkomst6);
 
-    System.out.println(7*getal= uitkomst7);
+    System.out.println(7 * int getal= int uitkomst7);
 
-    System.out.println(8*getal= uitkomst8);
+    System.out.println(8 * int getal= int uitkomst8);
 
-    System.out.println(9*getal= uitkomst9);
+    System.out.println(9 * int getal= int uitkomst9);
 
-    System.out.println(10*getal= uitkomst10);
+    System.out.println(10 * int getal= int uitkomst10);
 
+    System.out.println();
+    System.out.println("1 * 8= 8");
+    System.out.println("2 * 8= 16");
+    System.out.println("3 * 8= 24");
+    System.out.println("4 * 8= 32");
+    System.out.println("5 * 8= 40");
+    System.out.println("6 * 8= 48");
+    System.out.println("7 * 8= 56");
+    System.out.println("8 * 8= 64");
+    System.out.println("9 * 8= 72");
+    System.out.println("10 * 8= 80");
 }
 }
